bc26dc654bd9946a33b99b401837335009d091f4
didilmonteir <didilmonteiro70@gmail.com>
2018-05-18 11:39:09 -0300

    modificações (teste dando erro)

--- src/main/java/br/edu/ifal/academicsistemweb/repositorio/CursoRepositorio.java
+++ src/main/java/br/edu/ifal/academicsistemweb/repositorio/CursoRepositorio.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.edu.ifal.academicsistemweb.modelo.Curso;
 
 public interface CursoRepositorio extends JpaRepository<Curso, Integer>{
+	
 	List<Curso> findByNomeContaining(String nome);
 }
 
